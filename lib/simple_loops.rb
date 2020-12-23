df9726af01d86919d42fd7efe0987f4f44cac6d0
Joshua Morales <joshuapmorales@gmail.com>
2020-12-23 01:35:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,14 +2,7 @@
 
 def loop_message_five_times
   
-counter = 0 
-loop_message_five_times = ["Hello World."]
 
-while loop_message_five_times [counter]
-  puts loop_message_five_times [counter]
-  counter += 5
-  
-  end
   
 end
 
